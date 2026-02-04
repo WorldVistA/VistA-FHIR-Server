a8c9327a28a2ae5f46158b29f0d1499d37bd8e44
George Lilly <glilly@glilly.net>
2026-02-03 20:06:01 -0500

    Fix sdt

--- src/C0FHIRTS.m
+++ src/C0FHIRTS.m
@@ -1,15 +1,29 @@
-C0FHIRTS ;VAMC/JS-FHIR SUITE TESTER ; 30-JAN-2026
- ;;1.1;C0FHIR PROJECT;;Jan 30, 2026;Build 2
+C0FHIRTS ;VAMC/JS-FHIR SUITE TESTER ; 03-FEB-2026
+ ;;1.2;C0FHIR PROJECT;;Feb 3, 2026;Build 4
  Q
-EN N DFN,ENCPTR,RES,DIR,X,Y,SDT,EDT
+EN ; Main entry point
+ N DFN,DIC,Y,ENCPTR,RTN,I,SDT,EDT
+ S (SDT,EDT)="" ; Initialize to prevent Undefined Variable error
+ ;
  W !!,"--- C0FHIR Suite Tester ---",!
- S DIR(0)="PO^2:AEMQ",DIR("A")="Select Patient" D ^DIR Q:$D(DIRUT)  S DFN=+Y
- S DIR(0)="PO^409.68:AEMQ",DIR("A")="Select Encounter (Optional)",DIR("S")="I $P(^(0),U,2)=DFN"
- D ^DIR S ENCPTR=+Y
- I 'ENCPTR D
- . S DIR(0)="DO",DIR("A")="Start Date" D ^DIR S SDT=Y
- . S DIR(0)="DO",DIR("A")="End Date" D ^DIR S EDT=Y
- D GENFULL^C0FHIRGF(.RES,DFN,ENCPTR,SDT,EDT)
- I '$D(RES) W !,"No results." Q
- W !!,"JSON Preview:",! S I="" F  S I=$O(RES(I)) Q:I=""!(I>10)  W RES(I)
+ ; 1. Select Patient
+ S DIC="^DPT(",DIC(0)="AEMQ",DIC("A")="Select Patient: "
+ D ^DIC Q:Y<0
+ S DFN=+Y
+ ;
+ ; 2. Select Encounter (Optional)
+ S DIC="^SCE(",DIC(0)="AEMQ",DIC("A")="Select Encounter (Optional): "
+ S DIC("S")="I $P(^(0),U,2)=DFN" ; Only show encounters for this patient
+ D ^DIC
+ S ENCPTR=$S(Y>0:+Y,1:"")
+ ;
+ W !!,"Generating FHIR Bundle..."
+ ; 3. Invoke Aggregator
+ D GENFULL^C0FHIRGF(.RTN,DFN,ENCPTR,SDT,EDT)
+ ;
+ ; 4. Display JSON Output (First 20 lines)
+ W !!,"--- JSON Output Preview ---",!
+ S I="" F  S I=$O(RTN(I)) Q:I=""!(I>20)  D
+ . W RTN(I),!
+ W !,"... [Output Truncated] ..."
  Q
